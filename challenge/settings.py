45c3cd3ec89a3138683402440ed5f31d230a0a57
Alexei-Oficina <alexei.popov@towebs.com>
2020-11-25 10:52:20 -0300

    fix

--- challenge/settings.py
+++ challenge/settings.py
@@ -148,7 +148,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
